6c7ff0ea480bca23469a36cb7eec6b82b0e7cbd9
Eugene Khablenko <eugene.khablenko@gmail.com>
2024-04-24 23:00:38 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import programmer from './assets/images/programmer.gif';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>Frontend mentor</h1>
+    <img src={programmer} alt="Me" />
+    </>
+  )
+}
+
+export default App
