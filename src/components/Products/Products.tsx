48fa18f31bb12d81c6ef6e814773ba29f4a551a4
Eugene Khablenko <eugene.khablenko@nextiva.com>
2024-04-28 07:30:49 -0700

    added producs & services texts, added scss

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -4,8 +4,27 @@ export const Products = () => {
       <div className='container'>
         <div className='section__content'>
           <h2 className='section__title'>Які технології я викладаю</h2>
+          <p className='section__description'>
+            Загальний курс складається з наступних розділів:
+          </p>
+          <ul className='products__list'>
+            <li className='products__item'>HTML</li>
+            <li className='products__item'>CSS</li>
+            <li className='products__item'>JavaScript</li>
+            <li className='products__item'>React</li>
+            <li className='products__item'>Node.JS</li>
+          </ul>
+          <p className='section__description'>
+            Також у процесі курсу ми будемо розглядати такі теми:
+          </p>
+          <ul className='products__list'>
+            <li className='products__item'>GitHub</li>
+            <li className='products__item'>API</li>
+            <li className='products__item'>SQL</li>
+            <li className='products__item'>Bootstrap</li>
+          </ul>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './assets/styles/index.scss';
 import { HomePage } from './pages/HomePage/HomePage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<HomePage />);
