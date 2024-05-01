26060a0a523f553f9e905219a4cceae0196474ad
Eugene Khablenko <eugene.khablenko@nextiva.com>
2024-04-30 17:31:56 -0700

    added styles

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,22 +1,52 @@
+import './Portfolio.scss';
+import miami from '../../assets/images/portfolio/miami.jpg'
+import soul from '../../assets/images/portfolio/the-soul.jpg';
+import ecommerce from '../../assets/images/portfolio/ecommerce.jpg';
+
 export const Portfolio = () => {
   return (
     <section className='section portfolio'>
       <div className='container'>
         <div className='section__content'>
           <h2 className='section__title'>Мої роботи</h2>
-          <p className="section__description">
-            Нижче ти можеш подивитися на деякі мої роботі. Я не викладаю всі їх тут, щоб не перевантажувати тебе інформацією. Якщо тобі цікавий повний перелік моїх робіт - ти можеш ознайомитися з ними на моєму Гітхабі
+          <p className='section__description'>
+            Нижче ти можеш подивитися на деякі мої роботі. Я не викладаю всі їх
+            тут, щоб не перевантажувати тебе інформацією. Якщо тобі цікавий
+            повний перелік моїх робіт - ти можеш ознайомитися з ними на моєму
+            Гітхабі
           </p>
-          <ul className="section__list">
-            <li className="section__item">
-              <a href="" className="section__link">Лендінг "Miami Kings"</a>
-              </li>
-            <li className="section__item">
-              <a href="" className="section__link">Сайт інтернет-магазину мобільних пристроїв</a>
-              </li>
-            <li className="section__item">
-              <a href="" className="section__link">Багатосторінковий сайт ГО "Психологічний центр The Soul"</a>
-              </li>
+
+          <ul className='portfolio__list'>
+            <li className='portfolio__item'>
+                <h3 className="portfolio__item-title">Лендінг "Miami Kings"</h3>
+              <a href='' className='portfolio__link'>
+                <img
+                  src={miami}
+                  alt=''
+                  className='portfolio__image'
+                />
+              </a>
+            </li>
+            <li className='portfolio__item'>
+                <h3 className="portfolio__item-title">Інтернет-магазин мобільних пристроїв</h3>
+              <a href='' className='portfolio__link'>
+                <img
+                  src={ecommerce}
+                  alt=''
+                  className='portfolio__image'
+                />
+              </a>
+            </li>
+            <li className='portfolio__item'>
+                <h3 className="portfolio__item-title">Психологічний центр The Soul</h3>
+              <a href='' className='portfolio__link'>
+                <img
+                  src={soul}
+                  alt=''
+                  className='portfolio__image'
+                />
+              </a>
+            </li>
           </ul>
         </div>
       </div>
