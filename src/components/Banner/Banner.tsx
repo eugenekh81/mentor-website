56866f34ffcbbc90440154152d4d38decedc43a2
Eugene Khablenko <eugene.khablenko@nextiva.com>
2024-05-02 18:00:04 -0700

    added title animation, styles, icons

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,18 +1,28 @@
+import { useEffect, useRef } from 'react';
 import './Banner.scss';
-
+const titleText = ['П','р','и','в','і','т','!','<br />','<br />','М','е','н','е',' ','з','в','а','т','и', ' ', 'Є','в','г','е','н', '<br />','<br />','я', ' ', '-', ' ', 'м','е','н','т','о','р', ' ','з',' ','в','е','б','-','р','о','з','р','о','б', 'к','и'];
 export const Banner: React.FC = () => {
+const title = useRef<HTMLHeadingElement | null>(null);
+
+useEffect(() => {
+  if (title.current) {
+    for (let i = 0; i < titleText.length; i++) {
+        setTimeout(() => {
+          if (title.current) {
+            title.current.innerHTML += titleText[i];
+          }
+        }, 100 * i + 100 * Math.random());
+    }
+  }
+}, [title])
+
+
+
   return (
     <section className='section banner'>
       <div className='container'>
         <div className='banner__content'>
-          <h1 className='banner__title'>
-            Привіт!
-            <br />
-            <br />
-            Мене звати Євген,
-            <br />
-            <br />я - ментор з веб-розробки<span className='cursor'>&nbsp;</span>
-          </h1>
+          <h1 className='banner__title' ref={title}></h1>
         </div>
       </div>
     </section>
