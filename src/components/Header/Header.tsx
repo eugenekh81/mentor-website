673c745c7e8638846c5aa506306deab3fd28d6cd
Eugene Khablenko <eugene.khablenko@gmail.com>
2024-05-28 13:42:15 +0300

    added navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Header.scss';
 import sprite from '../../assets/images/icons/sprite.svg';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
 
 export const Header: React.FC = () => {
+  const { hash } = useLocation();
+
   return (
     <header className='header'>
       <div className='container'>
@@ -16,37 +20,72 @@ export const Header: React.FC = () => {
           <nav className='nav'>
             <ul className='nav__list'>
               <li className='nav__item'>
-                <a href='#services' className='nav__link'>
+                <a
+                  href='#services'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#services',
+                  })}
+                >
                   Допомога
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#products' className='nav__link'>
+                <a
+                  href='#products'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#products',
+                  })}
+                >
                   Технології
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#about' className='nav__link'>
+                <a
+                  href='#about'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#about',
+                  })}
+                >
                   Про мене
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#portfolio' className='nav__link'>
+                <a
+                  href='#portfolio'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#portfolio',
+                  })}
+                >
                   Порфтфоліо
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#reviews' className='nav__link'>
+                <a
+                  href='#reviews'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#reviews',
+                  })}
+                >
                   Відгуки
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#faq' className='nav__link'>
+                <a
+                  href='#faq'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#faq',
+                  })}
+                >
                   ЧаПи
                 </a>
               </li>
               <li className='nav__item'>
-                <a href='#contacts' className='nav__link'>
+                <a
+                  href='#contacts'
+                  className={classNames('nav__link', {
+                    'nav__link--active': hash === '#contacts',
+                  })}
+                >
                   Контакти
                 </a>
               </li>
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import './assets/styles/index.scss';
-import { HomePage } from './pages/HomePage/HomePage.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<HomePage />);
+import { HomePage } from './pages/HomePage';
+const router = createBrowserRouter([
+  {
+    path: '/mentor-website',
+    element: <HomePage />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />
+);
