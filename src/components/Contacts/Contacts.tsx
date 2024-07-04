670872a1493195f3037971f77260003279808359
Eugene Khablenko <eugene.khablenko@nextiva.com>
2024-07-04 13:31:45 -0700

    added contacts scss

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,10 +1,14 @@
+import './Contacts.scss';
+
 export const Contacts = () => {
   return (
     <section className='section contacts' id='contacts'>
       <div className='container'>
         <div className='section__content'>
           <h2 className='section__title'>Контакти</h2>
-          <a href='https://t.me/js_mentor'>Call me</a>
+          <a href='https://t.me/js_mentor' class='contacts__link'>
+            Call me
+          </a>
         </div>
       </div>
     </section>
