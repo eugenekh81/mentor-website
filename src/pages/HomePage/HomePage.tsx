6316bde4482d70281f2059a165b8795f0d705df1
Eugene Khablenko <eugene.khablenko@gmail.com>
2024-08-19 16:56:32 +0300

    mobile header & mobile banner done - polished

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
 
-import { Banner } from '../../components/Banner';
-import { Services } from '../../components/Services';
-import { Products } from '../../components/Products';
-import { About } from '../../components/About';
-import { Portfolio } from '../../components/Portfolio';
-import { Reviews } from '../../components/Reviews';
-import { Contacts } from '../../components/Contacts';
-import { FAQ } from '../../components/FAQ';
 import { Header } from '../../components/Header';
-import { Results } from '../../components/Results';
+import { Banner } from '../../components/Banner';
+// import { Services } from '../../components/Services';
+// import { Products } from '../../components/Products';
+// import { About } from '../../components/About';
+// import { Portfolio } from '../../components/Portfolio';
+// import { Reviews } from '../../components/Reviews';
+// import { Contacts } from '../../components/Contacts';
+// import { FAQ } from '../../components/FAQ';
+// import { Results } from '../../components/Results';
 
 export const HomePage: React.FC = () => {
   const [currentSection, setCurrentSection] = useState<string | null>(null);
