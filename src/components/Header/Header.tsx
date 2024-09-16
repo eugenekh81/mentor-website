24a1df46af711a96ba20e0c12d1a0d0a78fa7c63
Eugene Khablenko <eugene.khablenko@nextiva.com>
2024-09-16 13:11:38 -0700

    added animation to the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Header.scss';
+import css from './Header.module.scss';
+import nav from './Nav.module.scss';
 import cn from 'classnames';
 import { NavLink } from 'react-router-dom';
 import { Button } from '../Button';
@@ -12,115 +13,115 @@ type Props = {
 
 export const Header: React.FC<Props> = ({ currentSection, onSelect }) => {
   return (
-    <header className='header'>
+    <header className={css.header}>
       <div className='container'>
-        <div className='header__content'>
-          <a href='/mentor-website' className='header__logo logo'>
+        <div className={css.content}>
+          <a href='/mentor-website' className={cn(css.headerLogo, css.logo)}>
             <SVGIcon iconId='main-logo' />
           </a>
 
-          <nav className='nav'>
-            <ul className='nav__list'>
+          <nav className={nav.nav}>
+            <ul className={nav.list}>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('services');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#services',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#services',
                   })}
                 >
                   Допомога
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('products');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#products',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#products',
                   })}
                 >
                   Технології
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('about');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#about',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#about',
                   })}
                 >
                   Про мене
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('portfolio');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#portfolio',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#portfolio',
                   })}
                 >
                   Портфоліо
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('reviews');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#reviews',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#reviews',
                   })}
                 >
                   Відгуки
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('faq');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#faq',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#faq',
                   })}
                 >
                   ЧаПи
                 </NavLink>
               </li>
               <li
-                className='nav__item'
+                className={nav.item}
                 onClick={() => {
                   onSelect('contacts');
                 }}
               >
                 <NavLink
                   to=''
-                  className={cn('nav__link', {
-                    'nav__link--active': currentSection === '#contacts',
+                  className={cn(nav.link, {
+                    [nav.active]: currentSection === '#contacts',
                   })}
                 >
                   Контакти
@@ -129,15 +130,15 @@ export const Header: React.FC<Props> = ({ currentSection, onSelect }) => {
             </ul>
           </nav>
 
-          <div className='header__buttons'>
+          <div className={css.buttons}>
             <a
               href='https://t.me/js_mentor'
               target='_blank'
-              className='header__link tg'
+              className={cn('tg', css.link)}
             >
-              <SVGIcon className='header__icon' iconId='telegram' />
+              <SVGIcon className={css.icon} iconId='telegram' />
             </a>
-            <div className='header__button'>
+            <div className={css.button}>
               <Button variant='small'>Безкоштовний урок</Button>
             </div>
           </div>
